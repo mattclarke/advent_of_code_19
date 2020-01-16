f1d2d22d7acd5c31257874a2a2cbbed3f4080540
Matt Clarke <matt.clarke@esss.se>
2020-01-16 10:08:40 +0100

    Day 16 done - needed a clue

--- advent_16/solution.py
+++ advent_16/solution.py
@@ -16,15 +16,23 @@ def part_1():
         index = 0
         length = 1
         base_ = 1
+        prev = 0
 
         for _ in data:
             accum = 0
-
-            while index < len(data):
-                max_index = index + length if index + length < len(data) else len(data)
-                accum += sum(data[index:max_index]) * base_
-                base_ *= -1
-                index += length * 2
+            if index < len(data) // 2:
+                while index < len(data):
+                    max_index = index + length if index + length < len(data) else len(data)
+                    accum += sum(data[index:max_index]) * base_
+                    base_ *= -1
+                    index += length * 2
+            elif index == len(data) // 2:
+                accum += sum(data[index:])
+            else:
+                # After we reach halfway it is just 1s
+                # So we can short circuit it a bit
+                accum = prev - data[index-1]
+            prev = accum
             new_data.append(abs(accum) % 10)
             length += 1
             start_index += 1
@@ -37,37 +45,26 @@ def part_1():
 # Part 1 = 53296082
 part_1()
 
-data = ORIG_DATA[:]
 
-print(len(data))
-
-def part_2():
+def part_2_quicker():
+    # Because the offset is in the 2nd half of the data
+    # we are at a point where the mask is 000...000111...111
+    # This means we can throw a lot of data away
     data = ORIG_DATA[:] * 10000
+    offset = int(input_data[0:7])
+    print("Offset", offset)
 
     for _ in range(100):
-        new_data = []
-        start_index = 0
-        index = 0
-        length = 1
-        base_ = 1
-
-        for _ in data:
-            accum = 0
-
-            while index < len(data):
-                max_index = index + length if index + length < len(data) else len(data)
-                accum += sum(data[index:max_index]) * base_
-                base_ *= -1
-                index += length * 2
-            new_data.append(abs(accum) % 10)
-            length += 1
-            start_index += 1
-            index = start_index
+        new_data = [0] * len(data)
+        max_num = sum(data[offset:])
+        new_data[offset] = max_num % 10
+        for i in range(offset + 1, len(data)):
+            max_num -= data[i - 1]
+            new_data[i] = max_num % 10
+            
         data = new_data
-        print(data[0:32])
-
-    print("".join([str(x) for x in data[0:8]]))
+    print("".join([str(x) for x in data[offset:offset+8]]))
 
 
-offset = int(input_data[0:8])
-part_2()
+# Part 2 = 43310035
+part_2_quicker()
