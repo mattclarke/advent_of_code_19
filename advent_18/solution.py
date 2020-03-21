5e3ca8ad689337994ff1ad9f59754f6a1e8a92ad
Matt Clarke <matt.clarke@esss.se>
2020-03-21 13:30:57 +0100

    One way of doing part 1

--- advent_18/solution.py
+++ advent_18/solution.py
@@ -1,9 +1,125 @@
+from collections import deque
+
 with open("input_data.txt") as f:
-    input_data = f.readlines()
+    input_data = f.read()
 
 ORIG_DATA = []
-for row in input_data:
+for row in input_data.split():
+    print(row)
     r = [c for c in row if c != "\n"]
     ORIG_DATA.append(r)
 
-print(ORIG_DATA)
+
+class Node:
+    def __init__(self, content=None):
+        self.links = []
+        self.content = content
+
+
+KEYS = {}
+GATES = {}
+LOOKUP = {}
+START = None
+
+NODES = {}
+
+for y in range(len(ORIG_DATA)):
+    for x in range(len(ORIG_DATA[0])):
+        if ORIG_DATA[y][x] != "#":
+            if ORIG_DATA[y][x] == "@":
+                START = (x, y)
+                n = Node("@")
+            elif ORIG_DATA[y][x].isalpha():
+                n = Node(ORIG_DATA[y][x])
+                if ORIG_DATA[y][x].islower():
+                    KEYS[ORIG_DATA[y][x]] = (x, y)
+                else:
+                    GATES[ORIG_DATA[y][x]] = (x, y)
+            else:
+                n = Node()
+
+            # Check for links
+            if ORIG_DATA[y][x - 1] != "#":
+                n.links.append((x - 1, y))
+            if ORIG_DATA[y-1][x] != "#":
+                n.links.append((x, y-1))
+            if ORIG_DATA[y][x + 1] != "#":
+                n.links.append((x + 1, y))
+            if ORIG_DATA[y+1][x] != "#":
+                n.links.append((x, y+1))
+            NODES[(x, y)] = n
+
+
+print(KEYS)
+print(GATES)
+print(START)
+print(len(NODES))
+
+
+def eliminate_deadend(node, x, y):
+    prev = NODES[node.links[0]]
+    prev.links.remove((x, y))
+    if len(prev.links) == 1 and prev.content is None:
+        eliminate_deadend(prev, *node.links[0])
+    elif len(curr.links) == 1 and prev.content and prev.content.isupper():
+        del GATES[prev.content]
+        eliminate_deadend(prev, *node.links[0])
+    del NODES[(x, y)]
+
+# Second pass to remove any dead - ends
+for y in range(len(ORIG_DATA)):
+    for x in range(len(ORIG_DATA[0])):
+        if (x, y) in NODES:
+            curr = NODES[(x, y)]
+            if len(curr.links) == 1 and curr.content is None:
+                # Is a dead - end
+                eliminate_deadend(curr, x, y)
+            elif len(curr.links) == 1 and curr.content and curr.content.isupper():
+                # Is a dead - end lock so can be deleted
+                eliminate_deadend(curr, x, y)
+
+print(len(NODES))
+for y in range(len(ORIG_DATA)):
+    line = []
+    for x in range(len(ORIG_DATA[0])):
+        if (x, y) in NODES:
+            curr = NODES[(x, y)]
+            if curr.content:
+                line.append(curr.content)
+            else:
+                line.append(".")
+        else:
+            line.append("#")
+    print("".join(line))
+
+print(GATES)
+print(len(GATES))
+
+# Part 1 = 5402
+visit_stack = [((START[1], START[0]), 0, set())]
+seen = set()
+len_keys = 0
+
+# Avoid back-tracking and repeated visits to the same node
+# by stopping if the node and the collected keys are the same.
+while visit_stack:
+    node, dist, keys = visit_stack[0]
+    current = NODES[node]
+    visit_stack.pop(0)
+    unique = (node, tuple(sorted(keys)))
+    if unique in seen:
+        continue
+    seen.add(unique)
+    if current.content and current.content.isupper() and current.content.lower() not in keys:
+        continue
+    new_keys = set(keys)
+    if current.content and current.content.islower():
+        new_keys.add(current.content)
+        if len(new_keys) > len_keys:
+            len_keys = len(new_keys)
+            print(new_keys)
+        if len(new_keys) == len(KEYS):
+            print("Done: ", dist)
+            break
+    for k in current.links:
+        visit_stack.append((k, dist + 1, new_keys))
