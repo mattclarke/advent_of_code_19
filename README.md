5ccf19a55938bc11d80e295b79d00fe2b45da03e
Matt Clarke <matt.clarke@esss.se>
2020-04-13 17:25:47 +0200

    Day 25 done.

--- README.md
+++ README.md
@@ -1,7 +1,64 @@
 # Advent of Code 2019
 
+All 50 stars gained!
+
+Started using Rust as a learning exercise, but it was taking so long that I switched to Python.
+Note: expect to see very bad Rust code ;)
+
+## Day 20
+### Part 1
+* Basic shortest path calculation, just treat the portals as normal links.
+### Part 2
+* Have to keep track of the level of recursion we are in.
+* Other than that it is another shortest path calculation.
+* Uses `heapq` to make sure we avoid recursing too deep by always putting the lowest levels at the front of the queue.
+
+## Day 21
+### Part 1
+* If there are any holes between robot and D then jump.
+### Part 2
+* Needs to handle something like `#####.#.#...#####`, code from part 1 ends up on the first island.
+* Solution is to look ahead and only jump if the second jump is possible or we can take a step after the first jump.
+
+## Day 22
+### Part 1
+* Simple enough: can just create functions to do the three actions and do it for the whole deck.
+* Can be simplified to just track the card we are interested in (big speed-up)
+### Part 2
+* Found this really difficult as had no idea where to begin, so needed internet help. 
+* Great puzzle though - I learned a lot.
+* Easiest solution to understand:
+    * The simplified functions from part 1 are of the linear form ax + b give or take the mod stuff.
+    * Going through the commands is essentially h(g(f(x)) for however many commands there are.
+    * This means for part 1 it is possible to calculate a "total" a and b and just plug in x
+    * For part 2, we need to the inverse of the linear functions and to run through the commands backwards.
+    * The inverse of ax + b is y/a - b/a.
+    * The complication is for the inverse of the "deal" function because it needs an inverse mod function.
+        * 2019 * 500 % 10007 = 8800
+        * 8800 / 500 = 17.6 (not 2019)
+    * For the deal function we keep adding the total number of cards to y until y/a gives a whole number.
+        * n * NUM_CARDS * 8800 / 500 = 2019
+    * Finally we need to scale a and b by the number of shuffles.
+    * Because this is a big number we use "exponential by squaring" to speed it up.
+* Alternative method 1:
+    * Because the number of cards is prime we can use Fermat's little theorem to calculate the inverse mod.
+    * Fermat's little theorem says inv_mod = a ** (m-2) % m if m is prime
+    * So one 'deal 500' for part 1:
+        * 2019 * 500 % 10007 = 8800
+    * Inverse:
+        * 8800 * pow(500, 10005, 10007) % 10007 = 2019
+* Alternative method 2:
+    * Algebra!
+    * Take a and b from doing the calculation forwards
+    * Scaling up for the number of shuffles:
+        * a becomes a ** num_shuffles % num_cards
+        * b expands out into a geometric progression which can be solved but also requires inverse mod.
+        * Then the linear equation can be inverted to give the final number. Once again it requires an inverse mod.
+        * The inverse mods can be done using Fermat's little theorem
+        * This is a very elegant and quick way of solving - there is no way I would have stumbled across this.
+
 ## Day 23
-### Part  1
+### Part 1
 * Modified IntCode so that it returns when it has a message ready (i.e. output length = 3).
 * Created 50 IntCode computers (arrays of memories, indexes and relative bases).
 * Each computer gets its own queue.
@@ -16,7 +73,7 @@
 * Exit if the value sent is the same as the previously sent value.
 
 ## Day 24
-### Part  1
+### Part 1
 * Simple enough.
 * Run the rules and calculate the "hash", if it matches a previous hash then we are done.
 
@@ -25,3 +82,7 @@
 * Then recurse outwards (only goes up one level)
 * Then repeat starting from the new outermost level
 * Logically pretty straightforward but I made a mess of the recursion functions, so took a while to debug.
+
+## Day 25
+Just play it!
+:)
---
+++ advent_25/src/main.rs
@@ -0,0 +1,274 @@
+use std::char;
+use std::cmp;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+fn get_modes(command: i64, number: i64) -> Vec<i64> {
+    let mut c: i64 = command / 100;
+    let mut modes: Vec<i64> = Vec::new();
+
+    for _i in 0..number {
+        modes.push(c % 10);
+        c = c / 10;
+    }
+
+    return modes;
+}
+
+fn get_operands(
+    commands: &Vec<i64>,
+    modes: &Vec<i64>,
+    index: usize,
+    relative_base: i64,
+    number: i64,
+) -> Vec<i64> {
+    let mut operands: Vec<i64> = Vec::new();
+    let mut temp_index = index + 1;
+
+    for i in 0..number {
+        if modes[i as usize] == 0 {
+            // Position mode
+            operands.push(commands[commands[temp_index] as usize]);
+        } else if modes[i as usize] == 1 {
+            // Parameter mode
+            operands.push(commands[temp_index]);
+        } else if modes[i as usize] == 2 {
+            // Relative mode
+            let first = commands[(relative_base + commands[temp_index]) as usize];
+            operands.push(first);
+        }
+        temp_index += 1;
+    }
+    return operands;
+}
+
+fn get_write_index(commands: &Vec<i64>, mode: &i64, index: usize, relative_base: i64) -> usize {
+    if *mode == 0 {
+        // Position mode
+        // println!("output = position {}", index);
+        return commands[index] as usize;
+    } else if *mode == 1 {
+        // Parameter mode
+        // println!("output = parameter {}", index);
+        return commands[index] as usize;
+    } else {
+        // Relative mode
+        return (relative_base + commands[index]) as usize;
+    }
+}
+
+fn get_jump_index(commands: &Vec<i64>, mode: &i64, index: usize, relative_base: i64) -> usize {
+    if *mode == 0 {
+        // Position mode
+        // println!("output = position {}", index);
+        return commands[commands[index] as usize] as usize;
+    } else if *mode == 1 {
+        // // Parameter mode
+        // println!("output = parameter {}", index);
+        return commands[index] as usize;
+    } else {
+        // Relative mode
+        return commands[(relative_base + commands[index]) as usize] as usize;
+    }
+}
+
+fn run_computer_index(
+    commands: &mut Vec<i64>,
+    inputs: &mut Vec<i64>,
+    mut index: i64,
+    mut relative_base: i64,
+) -> (Vec<i64>, i64, i64) {
+    let mut output: Vec<i64> = Vec::new();
+
+    while commands[index as usize] != 99 {
+        let opcode = commands[index as usize] % 100;
+        if opcode == 1 {
+            // Addition
+            let modes = get_modes(commands[index as usize], 3);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 3);
+            let out_index =
+                get_write_index(commands, &modes[2], (index + 3) as usize, relative_base);
+            commands[out_index] = operands[0] + operands[1];
+            index += 4;
+        } else if opcode == 2 {
+            // Multiply
+            let modes = get_modes(commands[index as usize], 3);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 3);
+            let out_index =
+                get_write_index(commands, &modes[2], (index + 3) as usize, relative_base);
+            commands[out_index] = operands[0] * operands[1];
+            index += 4;
+        } else if opcode == 3 {
+            // Input
+            // println!("Inputs");
+            if inputs.len() == 0 {
+                // println!("Out of inputs");
+                return (output, index, relative_base);
+            }
+            let modes = get_modes(commands[index as usize], 1);
+            let out_index =
+                get_write_index(commands, &modes[0], (index + 1) as usize, relative_base);
+            commands[out_index] = inputs[0];
+            inputs.remove(0);
+            index += 2;
+        } else if opcode == 4 {
+            // Output
+            let modes = get_modes(commands[index as usize], 1);
+            let out_index =
+                get_jump_index(commands, &modes[0], (index + 1) as usize, relative_base);
+            output.push(out_index as i64);
+            index += 2;
+            // if output.len() == 3 {
+            //     // println!("Output = {:?}", output);
+            //     return (output, index, relative_base);
+            // }
+        } else if opcode == 5 {
+            // Jump-if-true
+            let modes = get_modes(commands[index as usize], 2);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 1);
+            let out_index =
+                get_jump_index(commands, &modes[1], (index + 2) as usize, relative_base);
+            if operands[0] != 0 {
+                index = out_index as i64;
+            } else {
+                index += 3;
+            }
+        } else if opcode == 6 {
+            // Jump-if-false
+            let modes = get_modes(commands[index as usize], 2);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 1);
+            let out_index =
+                get_jump_index(commands, &modes[1], (index + 2) as usize, relative_base);
+            if operands[0] == 0 {
+                index = out_index as i64;
+            } else {
+                index += 3;
+            }
+        } else if opcode == 7 {
+            // Less than
+            let modes = get_modes(commands[index as usize], 3);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 2);
+            let out_index =
+                get_write_index(commands, &modes[2], (index + 3) as usize, relative_base);
+            if operands[0] < operands[1] {
+                commands[out_index] = 1;
+            } else {
+                commands[out_index] = 0;
+            }
+            index += 4;
+        } else if opcode == 8 {
+            // Equal
+            let modes = get_modes(commands[index as usize], 3);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 2);
+            let out_index =
+                get_write_index(commands, &modes[2], (index + 3) as usize, relative_base);
+            if operands[0] == operands[1] {
+                commands[out_index] = 1;
+            } else {
+                commands[out_index] = 0;
+            }
+            index += 4;
+        } else if opcode == 9 {
+            // Adjust relative_base
+            let modes = get_modes(commands[index as usize], 1);
+            let operands = get_operands(commands, &modes, index as usize, relative_base, 1);
+            relative_base += operands[0];
+            index += 2;
+        }
+    }
+
+    return (output, index, relative_base);
+}
+
+fn string_to_ints(str: String, sep: char) -> Vec<i64> {
+    let parts = str.split(sep);
+    let vec = parts.flat_map(|x| x.parse::<i64>()).collect::<Vec<i64>>();
+    return vec;
+}
+
+fn read_file() -> String {
+    let mut file = File::open("input_data.txt").expect("No such file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    return contents;
+}
+
+fn print_response(output: Vec<i64>) -> String {
+    println!("===============");
+    let mut strs: Vec<String> = Vec::new();
+    let mut str = String::from("");
+    for i in output {
+        let c: u8 = i as u8;
+        print!("{}", c as char);
+        if c as char != '\n' {
+            str.push(c as char);
+        }
+    }
+    println!("===============");
+    return str;
+}
+
+fn append_command(inputs: &mut Vec<i64>, command: &String) {
+    for b in command.as_bytes() {
+        inputs.push(*b as i64);
+    }
+    inputs.push(10);
+}
+
+struct Room {
+    doors: Vec<String>,
+    items: Vec<String>,
+    door_index: usize,
+}
+
+fn part_1(orig_codes: &Vec<i64>) {
+    let mut memory = orig_codes.to_vec();
+    let mut inputs: Vec<i64> = vec![];
+    let mut index = 0;
+    let mut relative_base = 0;
+
+    // Get input prompt
+    let mut output = run_computer_index(&mut memory, &mut inputs, index, relative_base);
+    index = output.1;
+    relative_base = output.2;
+
+    print_response(output.0);
+
+    loop {
+        let stdin = io::stdin();
+        let mut input = &mut String::new();
+        stdin.read_line(input);
+        input.truncate(input.len() - 1);
+
+        append_command(&mut inputs, &input);
+        output = run_computer_index(&mut memory, &mut inputs, index, relative_base);
+        index = output.1;
+        relative_base = output.2;
+        print_response(output.0);
+    }
+}
+
+
+fn part_2(orig_codes: &Vec<i64>) {
+
+}
+
+fn main() -> std::io::Result<()> {
+    let contents = read_file();
+    let orig_codes = string_to_ints(contents, ',');
+
+    let mut memory = vec![0; orig_codes.len() * 100];
+    for i in 0..orig_codes.len() {
+        memory[i] = orig_codes[i];
+    }
+
+    // Part 1 =
+    part_1(&memory);
+
+    // Part 2 =
+    // part_2(&memory);
+
+    Ok(())
+}
